eaa78bb05ab80886c85f2bacac9326e0783bc720
Alexandra Ash <alexandraleah@gmail.com>
2018-06-15 14:21:31 -0400

    started working on models

--- models/index.js
+++ models/index.js
@@ -1,4 +1,9 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/wikistack');
 module.exports = { db };
-const Page = db.define('pages', {});
+const Page = db.define('pages', {
+  title: Sequelize.STRING,
+  slug: Sequelize.STRING,
+  content: Sequelize.STRING,
+  status: Sequelize.BOOLEAN,
+});
