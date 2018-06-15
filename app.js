3902568a96eb0b77999bbe1d93023040133695dc
Alexandra Ash <alexandraleah@gmail.com>
2018-06-15 12:05:40 -0400

    set up app

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const index = require('./views/index');
+console.log(index);
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+  res.send(index.main());
+});
 
 app.listen(1500, () => {
-  console.log('App is listening on port 1500')
-} );
+  console.log('App is listening on port 1500');
+});
